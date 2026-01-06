3dd92e55e4a1dc0e50652e690ecc18d5f4545e24
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-07 00:51:24 +0200

    Create apps.py

---
+++ properties/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class PropertiesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "properties"
+
+    def ready(self):
+        # Import signals to enable cache invalidation
+        from . import signals  # noqa: F401
