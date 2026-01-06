4378d4347f7f8ea6f29f3ce10d14cc07754262ae
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-07 00:54:45 +0200

    Create admin.py

---
+++ properties/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Property
+
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "location", "price", "created_at")
+    search_fields = ("title", "location")
