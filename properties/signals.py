5816b0c19c74d3e578c1f63d107a3a96124241c3
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-07 00:54:20 +0200

    Create signals.py

---
+++ properties/signals.py
@@ -0,0 +1,18 @@
+from django.core.cache import cache
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import Property
+
+
+CACHE_KEY = "all_properties"
+
+
+@receiver(post_save, sender=Property)
+def invalidate_cache_on_save(sender, instance, **kwargs):
+    cache.delete(CACHE_KEY)
+
+
+@receiver(post_delete, sender=Property)
+def invalidate_cache_on_delete(sender, instance, **kwargs):
+    cache.delete(CACHE_KEY)
